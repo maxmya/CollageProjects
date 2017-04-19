806cdcd0a81624292cbd28322c721fb520a98df8
Maxmya <maxmya@outlook.com>
2017-04-19 18:38:34 +0200

    UPDATE

--- Spring 2017/AI/Lab 4/script.pl
+++ Spring 2017/AI/Lab 4/script.pl
@@ -2,12 +2,18 @@ edge(a,b).
 edge(a,c).
 edge(b,d).
 edge(d,e).
-edge(d,c).
+edge(c,d).
 edge(c,f).
 edge(f,a).
 
-path(X,Y,Z):-
-  edge(X,Y),Z is [X,Y].
+path(X,Y):-
+  edge(X,Y).
 
- path(X,Y,A):-
+ path(X,Y):-
     edge(X,Z),path(Z,Y).
+
+
+
+    paths(X,X,[X]).
+    paths(X,Y,[X,Y]):-edge(X,Y).
+    paths(X,Y,List):-edge(X,Z),paths(Z,Y,L),append([X,Z],L,List).
