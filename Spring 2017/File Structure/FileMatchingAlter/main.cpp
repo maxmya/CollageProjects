50d14446ee83075b461c22515a5b389ad9467222
Maxmya <maxmya@outlook.com>
2017-04-23 21:21:17 +0200

    New Update

--- Spring 2017/File Structure/FileMatchingAlter/main.cpp
+++ Spring 2017/File Structure/FileMatchingAlter/main.cpp
@@ -1,6 +1,55 @@
+
+
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    string myPath = "//media//maxmya//Projects//Collage Projects//CollageProjects//Spring 2017//File Structure//FileMatchingAlter//";
+
+    ifstream inOldMaster(myPath + "oldmast.txt", ios::in);
+    ifstream inTransaction(myPath + "trans.txt", ios::in);
+    ofstream outNewMaster(myPath + "newmast.txt", ios::out);
+
+
+    if (!outNewMaster && !inOldMaster && !inTransaction) {
+        cerr << "files could not be opened" << endl;
+        exit(1);
+    }
+
+    int transAccountNum = 0;
+    int oldMastAccountNum = 0;
+    double dollarAmount;
+    double currentBalance;
+    string firstName;
+    string secondName;
+
+
+    while (!inOldMaster.eof() && !inTransaction.eof()) {
+
+        inOldMaster >> oldMastAccountNum >> firstName >> secondName >> currentBalance;
+
+        if (transAccountNum < oldMastAccountNum)
+
+            inTransaction >> transAccountNum >> dollarAmount;
+
+        while ((oldMastAccountNum == transAccountNum) && !inTransaction.eof()) {
+
+            currentBalance += dollarAmount;
+
+            inTransaction >> transAccountNum >> dollarAmount;
+        }
+        if (((oldMastAccountNum > transAccountNum) && !inTransaction.eof()) || inOldMaster.eof()) {
+            cerr << "Unmatched transaction record for account number: " << transAccountNum << endl;
+        }
+
+
+        if (!inOldMaster.eof())
+            outNewMaster << oldMastAccountNum << " " << firstName << " " << secondName << " " << currentBalance << endl;
+    }
     return 0;
 }
+
