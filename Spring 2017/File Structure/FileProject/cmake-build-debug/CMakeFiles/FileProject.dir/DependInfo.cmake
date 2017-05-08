e0db6dbeee54d97c3254b2812b357d5459f1039d
Maxmya <maxmya@outlook.com>
2017-05-08 17:07:08 +0200

    Updates

---
+++ Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/ClientData.cpp" "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/ClientData.cpp.o"
+  "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/IndexRecord.cpp" "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/IndexRecord.cpp.o"
+  "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/SecondaryRecord.cpp" "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/SecondaryRecord.cpp.o"
+  "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/main.cpp" "/media/maxmya/Projects/Collage Projects/CollageProjects/Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Spring 2017/File Structure/FileProject/cmake-build-debug/CMakeFiles/FileProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileProject.dir/main.cpp.o"
+  "CMakeFiles/FileProject.dir/SecondaryRecord.cpp.o"
+  "CMakeFiles/FileProject.dir/IndexRecord.cpp.o"
+  "CMakeFiles/FileProject.dir/ClientData.cpp.o"
+  "FileProject.pdb"
+  "FileProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
